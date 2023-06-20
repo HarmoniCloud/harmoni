49fde0d9a4cfeac37e40d2c10a2a66efcd3dfb4d
orig <oriorigranot@gamil.com>
2023-06-20 21:33:14 +0300

    feat(sdk): add Azure SDK dependencies to package.json feat(azure.provider.ts): add Azure SDK authentication and resource management client to AzureProvider class to enable fetching of resources from Azure subscription.

--- packages/sdk/src/providers/azure/azure.provider.ts
+++ packages/sdk/src/providers/azure/azure.provider.ts
@@ -1,8 +1,22 @@
 import { Provider } from '../provider';
 import { Provider as ProviderEnum } from '@harmoni/types';
+import { DefaultAzureCredential } from '@azure/identity';
+import { ResourceManagementClient } from '@azure/arm-resources';
 
 export class AzureProvider extends Provider {
+  private readonly credential: DefaultAzureCredential;
+
   constructor() {
     super(ProviderEnum.AZURE);
+
+    // TODO: Need to get AZURE_TENANT_ID, AZURE_CLIENT_ID, AZURE_CLIENT_SECRET from env
+    // Waiting for @slagter to create an Azure AD app for this purpose
+    this.credential = new DefaultAzureCredential();
+  }
+
+  async getResources(subscriptionId: string) {
+    const client = new ResourceManagementClient(this.credential, subscriptionId);
+    const resources = client.resources.list();
+    return resources;
   }
 }

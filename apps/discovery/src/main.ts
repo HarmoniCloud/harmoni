a59ad48070b4f00273bed14b138e7a52110c94cd
orig <48911144+origranot@users.noreply.github.com>
2023-06-23 23:29:09 +0300

    feat(discovery): add AppModule and ProviderModule to enable scheduling of tasks (#10)
    refactor(discovery): move AppModule to root directory for better project structure
    
    Co-authored-by: orig <oriorigranot@gamil.com>

--- apps/discovery/src/main.ts
+++ apps/discovery/src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
+import { AppModule } from './app.module';
 import { logger } from '@harmoni/logger';
 import { config } from '@harmoni/config';
 
